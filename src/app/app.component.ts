39ae5d654cf8f401dfc1196df8cff67269bc5ac9
mrivasfe <113179276+mrivasfe@users.noreply.github.com>
2022-09-14 17:10:38 +0200

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,18 +21,3 @@ export class AppComponent {
   }
 }
 
-//EJERCICIO 2:
-
-// 1) Convertimos este componente (app.component.ts) en
-// un componente multi-arcivho (creamos y rellenamos los archivos de template y de css) OJO: cambia los atributos
-// del decorador
-
-//2) Creamos el archivo app.component.html (vista/template)
-
-//3) Copiamos el contenido (sin las comillas exteriores) del atributo template dentro del arhivo html
-
-//4) Creamos ahora el archivo app.component.css (copiamos el contenido del atributo styles, sin las comillas exteriores, dentro de este archivo)
-
-//5) Cambiar el atributo template -> templateUrl: "app.component.html"
-
-//6) Cambiamos el atributo styles -> styleUrls:
--- src/app/temperatura/temperatura.component.ts
+++ src/app/temperatura/temperatura.component.ts
@@ -11,12 +11,12 @@ export class TemperaturaComponent implements OnInit {
   @Input() temp: number;
 
   ngOnInit() {}
-
-  //funciones
+  
   @Output() riseTempEvent = new EventEmitter(); //creacion de evento propio
   @Output() downTempEvent = new EventEmitter();
   @Output() resetTempEvent = new EventEmitter();
 
+  /*
   riseTempInvoke() {  //Llamador de evento
     this.riseTempEvent.emit();
   }
@@ -24,8 +24,25 @@ export class TemperaturaComponent implements OnInit {
   downTempInvoke() {
     this.downTempEvent.emit();
   }
-
+  
   resetTempInvoke() {
     this.resetTempEvent.emit();
+  }
+  */
+
+  @Output() tempChange = new EventEmitter<number>();
+
+  riseTempInvoke() {  //Llamador de evento para incrementar
+    this.tempChange.emit(this.temp + 1);
+  }
+
+  downTempInvoke() {  //Llamador de evento para decrementar
+    this.tempChange.emit(this.temp - 1);
+  }
+  //var = variable global
+  //let = variable local en la funcion
+  updateTempInvoke(param: string) {   //Funcion update enlazada a ngModelChange
+    let paramNumber: number = +param;
+    this.tempChange.emit(paramNumber);
   }
 }
